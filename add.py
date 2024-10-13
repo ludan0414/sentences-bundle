326ee8ff28f2d9c0fc927657bcdcb3493c10f8b6
ludan0414 <luan15801586800@outlook.com>
2024-10-13 14:39:18 +0800

    修复add.py的bug，添加说话人功能

--- add.py
+++ add.py
@@ -3,20 +3,28 @@ import json
 mydict = []
 with open('add.txt','r',encoding='utf8') as fp:
     while True:
-        s = fp.readline()
+        s = fp.readline().replace("\n","")
+        while s == '':
+            s = fp.readline().replace("\n","")
         if s == '-1':
             break
         if s == '-2':
             mydict.pop()
             continue
-        o = fp.readline()
-        fp.readline()
+        o = fp.readline().replace("\n","")
+        t = fp.readline().replace("\n","")
+        if t != '':
+            fp.readline().replace("\n","")
         tmp = dict()
-        tmp['hitokoto'] = s.replace("\n","")
+        tmp['hitokoto'] = s
         tmp['type'] = 'm'
-        tmp['from'] = o.replace("\n","")
-        tmp['from_who'] = None
+        tmp['from'] = o
+        if t != '':
+            tmp['from_who'] = t
+        else:    
+            tmp['from_who'] = None
         mydict.append(tmp)
+        #print(tmp)
     
 with open('./add.json','w',encoding='utf8',newline='\n') as f:
     data = json.dumps(mydict,indent=1,ensure_ascii=False)
